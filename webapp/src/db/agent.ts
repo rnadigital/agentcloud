ae6b551b9ab63cae284f9446295c2c1b4e17ca50
Thomas Lynch <thomas@rna.digital>
2023-10-25 17:53:16 +1100

    expoty -> export

--- webapp/src/db/agent.ts
+++ webapp/src/db/agent.ts
@@ -6,7 +6,7 @@ import toObjectId from '../lib/misc/toobjectid';
 
 export type AgentType = 'UserProxyAgent' | 'AssistantAgent';
 export type LlmConfigType = 'gpt-4-32k' | 'gpt-4-32k-0314';
-expoty type HumanInputModeType = 'ALWAYS' | 'NEVER' | 'TERMINAL';
+export type HumanInputModeType = 'ALWAYS' | 'NEVER' | 'TERMINAL';
 export type CodeExecutionConfigType = {
 	lastNMessages: number;
 	workDirectory: string;
