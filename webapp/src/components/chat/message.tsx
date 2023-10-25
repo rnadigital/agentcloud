b9df94a1db2a5b8fcdba76d05855a30cb3dc6548
Thomas Lynch <thomas@rna.digital>
2023-10-25 13:54:56 +1100

    Make chat message code blocks expand/collapse depending on length (>10 lines), and wrap long lines

--- webapp/src/components/chat/message.tsx
+++ webapp/src/components/chat/message.tsx
@@ -1,10 +1,7 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import React, { useState, useEffect } from 'react';
 import { ClipboardDocumentIcon } from '@heroicons/react/20/solid';
-import dynamic from 'next/dynamic';
-const Markdown = dynamic(() => import('react-markdown'), {
-	loading: () => <p>Loading...</p>
-});
+import Markdown from 'react-markdown';
 import { toast } from 'react-toastify';
 
 export function CopyToClipboardButton({ dataToCopy }) {
@@ -28,24 +25,39 @@ export function CopyToClipboardButton({ dataToCopy }) {
 }
 
 function MessageBody({ message, messageType, messageLanguage, style }) {
+	const messageContent = messageLanguage === 'json'
+		? JSON.stringify(message, null, '\t')
+		: message.toString();
+	const isLongMessage = messageContent
+		&& typeof messageContent.split === 'function'
+		&& messageContent.split(/\r?\n/).length > 10;
+	const [ collapsed, setCollapsed ] = useState(isLongMessage);
 	switch(messageType) {
 		case 'code':
 			return <>
-				<span className='h-8 bg-gray-700 p-2 text-white font-semibold w-full block text-xs ps-2 flex justify-between'>
+				<span className='h-8 bg-gray-700 p-2 text-white w-full block text-xs ps-2 flex justify-between'>
 					{messageLanguage}
-					<CopyToClipboardButton dataToCopy={messageLanguage === 'json' ? JSON.stringify(message, null, '\t') : message.toString()} />
+					<CopyToClipboardButton dataToCopy={messageContent} />
 				</span>
 				<div className='overlay-container'>
 					<SyntaxHighlighter
-						className='overlay-gradient'
+						wrapLongLines
+						className={collapsed ? 'overlay-gradient' : null}
 						language={messageLanguage}
 						style={style}
 						showLineNumbers={true}
-						customStyle={{ margin: 0 }}
+						customStyle={{ margin: 0, maxHeight: collapsed ? '10em' : 'unset' }}
 					>
-						{messageLanguage === 'json' ? JSON.stringify(message, null, '\t') : message}
+						{messageContent}
 					</SyntaxHighlighter>
-					<button className='overlay-button'>Click Me</button>
+					<button
+						className='overlay-button'
+						onClick={() => {
+							setCollapsed(oldCollapsed => !oldCollapsed);
+						}}
+					>
+						{collapsed ? 'Expand' : 'Collapse'}
+					</button>
 				</div>
 			</>;
 		case 'text':
@@ -62,7 +74,6 @@ export function Message({ message, messageType, messageLanguage, isFeedback, dat
 	: { message?: any, messageType?: string, messageLanguage?: string, isFeedback?: boolean, date?: Date, authorName?: string, authorImage?: string, incoming?: boolean }) {
 
 	const [ style, setStyle ] = useState(null);
-	const [ collapsed, setCollapsedCode ] = useState(null);
 	useEffect(() => {
 		if (!style) {
 			import('react-syntax-highlighter/dist/esm/styles/prism/material-dark')
