78ca7cfa5db9d25078a24866255f8907248d6e07
Thomas Lynch <thomas@rna.digital>
2023-10-25 17:13:23 +1100

    do the thing -> start settion ref #3

--- webapp/src/components/StartSessionChatbox.tsx
+++ webapp/src/components/StartSessionChatbox.tsx
@@ -84,7 +84,7 @@ export default function StartSessionChatbox() {
 									type='submit'
 									className='inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
 								>
-									Do the thing!
+									Start Session
 								</button>
 							</div>
 						</div>
