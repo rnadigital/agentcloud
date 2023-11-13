ca0129db61b4e549a89297968f2d427a66f000fc
Thomas Lynch <thomas@rna.digital>
2023-11-13 12:07:26 +1100

    Add posthog (optional with env var)

--- webapp/src/pages/_app.tsx
+++ webapp/src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import { useEffect } from 'react';
 import Layout from '../components/Layout';
 import 'nprogress/nprogress.css';
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import Head from 'next/head';
 import './globals.css';
 import { AccountWrapper } from '../context/account';
 import { SocketWrapper } from '../context/socket';
@@ -9,6 +11,22 @@ import { ChatWrapper } from '../context/chat';
 import React, { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+		api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+		// Enable debug mode in development
+		loaded: (posthog) => {
+			if (process.env.NODE_ENV === 'development') {
+				posthog.debug();
+			}
+		},
+		capture_pageview: false // Disable automatic pageview capture, as we capture manually
+	});
+}
   
 NProgress.configure({ showSpinner: false });
 // const loadRoutes = ['/login', '/register', '/changepassword', '/'];
@@ -19,30 +37,44 @@ Router.events.on('routeChangeComplete', (url) => NProgress.done());
 Router.events.on('routeChangeError', (_url) => NProgress.done());
 
 export default function App({ Component, pageProps }) {
+
+	useEffect(() => {
+		if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+			// Track page views
+			const handleRouteChange = () => posthog?.capture('$pageview');
+			Router.events.on('routeChangeComplete', handleRouteChange);
+			return () => {
+				Router.events.off('routeChangeComplete', handleRouteChange);
+			};
+		}
+	}, []);
+
 	const [pagePropsState] = useState(pageProps);
 	return (
-		<AccountWrapper pageProps={pagePropsState}>
-			<ChatWrapper>
-				<SocketWrapper>
-					<ToastContainer
-						progressClassName='toast-container'
-						bodyClassName='toast-body'
-						theme='colored' 
-						position='top-right'
-						autoClose={4000}
-						newestOnTop={true}
-						pauseOnFocusLoss={false}
-						pauseOnHover={false}
-						hideProgressBar={true}
-					/>
-					<Layout {...pagePropsState}>
-						<style>
-							{''}
-						</style>
-						<Component {...pagePropsState} />
-					</Layout>
-				</SocketWrapper>
-			</ChatWrapper>
-		</AccountWrapper>
+		<PostHogProvider client={posthog}>
+			<AccountWrapper pageProps={pagePropsState}>
+				<ChatWrapper>
+					<SocketWrapper>
+						<ToastContainer
+							progressClassName='toast-container'
+							bodyClassName='toast-body'
+							theme='colored'
+							position='top-right'
+							autoClose={4000}
+							newestOnTop={true}
+							pauseOnFocusLoss={false}
+							pauseOnHover={false}
+							hideProgressBar={true}
+						/>
+						<Layout {...pagePropsState}>
+							<style>
+								{''}
+							</style>
+							<Component {...pagePropsState} />
+						</Layout>
+					</SocketWrapper>
+				</ChatWrapper>
+			</AccountWrapper>
+		</PostHogProvider>
 	);
 }
