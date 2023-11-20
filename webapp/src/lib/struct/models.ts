53a471fdb08873da9c300041d71f0ff59f1775a4
Thomas Lynch <thomas@rna.digital>
2023-11-20 12:25:23 +1100

    remove gpt-4-32k model option

--- webapp/src/lib/struct/models.ts
+++ webapp/src/lib/struct/models.ts
@@ -4,6 +4,6 @@ import { CredentialPlatform } from './credentials';
 
 //TODO: types
 export const ModelList = {
-	[CredentialPlatform.OPENAI]: ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-32k', 'gpt-4-1106-preview'],
-	[CredentialPlatform.AZURE]: ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-32k', 'gpt-4-1106-preview'],	
+	[CredentialPlatform.OPENAI]: ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-1106-preview'],
+	[CredentialPlatform.AZURE]: ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-1106-preview'],	
 };
