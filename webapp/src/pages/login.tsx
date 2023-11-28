55068efaf33f791f71d0f9f6f2baf5348de058c5
Thomas Lynch <thomas@rna.digital>
2023-11-28 16:01:49 +1100

    Allow to disable the google/github buttons on frontend with a build var and change the button to 1 column

--- webapp/src/pages/login.tsx
+++ webapp/src/pages/login.tsx
@@ -137,9 +137,9 @@ export default function Login() {
 								</div>
 							</div>
 
-							<div className='mt-6 grid grid-cols-2 gap-4'>
+							<div className={`mt-6 grid grid-cols-${process.env.NEXT_PUBLIC_ENABLE_GOOGLE_OAUTH && process.env.NEXT_PUBLIC_ENABLE_GITHUB_OAUTH ? '2' : '1'} gap-4`}>
 
-								<Link
+								{process.env.NEXT_PUBLIC_ENABLE_GOOGLE_OAUTH && <Link
 									href='/auth/google'
 									className='flex w-full items-center justify-center gap-3 rounded-md bg-[#4285F4] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#4285F4]'
 								>
@@ -151,9 +151,9 @@ export default function Login() {
 										<path d='M881 442.4H519.7v148.5h206.4c-8.9 48-35.9 88.6-76.6 115.8-34.4 23-78.3 36.6-129.9 36.6-99.9 0-184.4-67.5-214.6-158.2-7.6-23-12-47.6-12-72.9s4.4-49.9 12-72.9c30.3-90.6 114.8-158.1 214.7-158.1 56.3 0 106.8 19.4 146.6 57.4l110-110.1c-66.5-62-153.2-100-256.6-100-149.9 0-279.6 86-342.7 211.4-26 51.8-40.8 110.4-40.8 172.4S151 632.8 177 684.6C240.1 810 369.8 896 519.7 896c103.6 0 190.4-34.4 253.8-93 72.5-66.8 114.4-165.2 114.4-282.1 0-27.2-2.4-53.3-6.9-78.5z' />
 									</svg>
 									<span className='text-sm font-semibold leading-6'>Google</span>
-								</Link>
+								</Link>}
 
-								<Link
+								{process.env.NEXT_PUBLIC_ENABLE_GITHUB_OAUTH && <Link
 									href='/auth/github'
 									className='flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]'
 								>
@@ -165,7 +165,8 @@ export default function Login() {
 										/>
 									</svg>
 									<span className='text-sm font-semibold leading-6'>GitHub</span>
-								</Link>
+								</Link>}
+								
 							</div>
 						</div>
 					</div>
