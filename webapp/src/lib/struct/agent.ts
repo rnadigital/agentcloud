4d360159cbf6829a8bc79d541670ea330968faa3
Thomas Lynch <thomas@rna.digital>
2023-12-12 10:28:03 +1100

    Move some agent code to struct lib for easier frontend sharing

--- webapp/src/lib/struct/agent.ts
+++ webapp/src/lib/struct/agent.ts
@@ -1,5 +1,29 @@
 'use strict';
 
+import { ObjectId } from 'mongodb';
+
+export type HumanInputModeType = 'ALWAYS' | 'NEVER' | 'TERMINAL';
+
+export type CodeExecutionConfigType = {
+	lastNMessages: number;
+	workDirectory: string;
+};
+
+export type Agent = {
+	_id?: ObjectId;
+	orgId?: ObjectId;
+	teamId?: ObjectId;
+	name: string;
+	type: AgentType;
+	codeExecutionConfig?: CodeExecutionConfigType;
+	systemMessage: string;
+	humanInputMode: HumanInputModeType;
+	credentialId: ObjectId;
+	toolIds?: ObjectId[];
+	datasourceIds?: ObjectId[];
+	model: string;
+};
+
 export enum AgentType {
 	USER_PROXY_AGENT = 'UserProxyAgent',
 	ASSISTANT_AGENT = 'AssistantAgent',
