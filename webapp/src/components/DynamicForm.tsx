249f20b54fc7b85577b6bef2edb6b6da4b49154c
Thomas Lynch <thomas@rna.digital>
2023-12-13 11:48:48 +1100

    Add support for array type fields with add/remove buttons in DynamicForm

--- webapp/src/components/DynamicForm.tsx
+++ webapp/src/components/DynamicForm.tsx
@@ -17,10 +17,29 @@ export default function DynamicForm({ spec }) {
 	const [formData, setFormData] = useState({});
 	const [arrayParams, setArrayParams] = useState([]);
 	const [showOptional, setShowOptional] = useState(false);
-	const [selectedOneOf, setSelectedOneOf] = useState(null);
+	const [oneOfState, setOneOfState] = useState(null);
+	const [arrayParamState, setArrayParamState] = useState(null);
 
-	const handleChange = (e) => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+	const handleChange = (event, key, index = null) => {
+		if (index !== null) {
+			const updatedArray = [...formData[key]];
+			updatedArray[index] = { ...updatedArray[index], [event.target.name]: event.target.value };
+			setFormData({ ...formData, [key]: updatedArray });
+		} else {
+			setFormData({ ...formData, [key]: event.target.value });
+		}
+	};
+
+	const handleAddArrayItem = (e, key) => {
+		e.preventDefault();
+		const updatedArray = formData[key] ? [...formData[key], {}] : [{}];
+		setFormData({ ...formData, [key]: updatedArray });
+	};
+
+	const handleRemoveArrayItem = (e, key, index) => {
+		e.preventDefault();
+		const updatedArray = formData[key].filter((_, i) => i !== index);
+		setFormData({ ...formData, [key]: updatedArray });
 	};
 
 	const handleToggleOptionalFields = () => {
@@ -44,8 +63,8 @@ export default function DynamicForm({ spec }) {
 	const renderOneOfOptions = (key, field) => {
 		return (
 			<select onChange={(e) => {
-				setSelectedOneOf(oldSelectedOneOf => ({
-					...oldSelectedOneOf,
+				setOneOfState(oldoneOfState => ({
+					...oldoneOfState,
 					[key]: e.target.value,
 				}));
 			}} className='block w-full p-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm'>
@@ -57,16 +76,36 @@ export default function DynamicForm({ spec }) {
 		);
 	};
 
+	const renderArrayField = (key, field) => {
+		return (
+			<div key={key} className='p-5 border m-4 me-0 rounded space-y-4'>
+				{formData[key] && formData[key].map((item, index) => (
+					<div key={index} className='ms-2 p-2 border-b'>
+						{renderFormFields(field.items.properties, field.items, false, true)}
+						<button onClick={(e) => handleRemoveArrayItem(e, key, index)} className='mt-4 mb-2 rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'>
+Remove
+						</button>
+					</div>
+				))}
+				<button onClick={(e) => handleAddArrayItem(e, key)} className='rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600'>
+Add Item
+				</button>
+			</div>
+		);
+	};
+
 	const renderInputField = (key, field, required = false) => {
 		console.log(key, field);
 		switch (true) {
 			case (field.type === 'object' && field.oneOf != null):
 				return <>
 					{renderOneOfOptions(key, field)}
-					{selectedOneOf
-						&& selectedOneOf[key]
-						&& renderFormFields(field.oneOf[parseInt(selectedOneOf[key])].properties, field.oneOf[parseInt(selectedOneOf[key])], true)}
+					{oneOfState
+						&& oneOfState[key]
+						&& renderFormFields(field.oneOf[parseInt(oneOfState[key])].properties, field.oneOf[parseInt(oneOfState[key])], true)}
 				</>;
+			case (field.type === 'array' && field?.items?.properties != null):
+				return renderArrayField(key, field);
 			case field.type === 'boolean':
 				return (
 					<input
@@ -80,14 +119,14 @@ export default function DynamicForm({ spec }) {
 			case field.type === 'string':
 			case field.type === 'integer':
 			default:
-				const InputComponent = field.examples != null ? 'textarea' : 'input';
+				const InputComponent = (field.type !== 'integer' && field.examples != null) ? 'textarea' : 'input';
 				return (
 					<InputComponent
 						key={key}
 						id={key}
+						type={field.type === 'integer' ? 'number' : 'text'}
 						rows={field.examples?.length > 0 && field.examples[0].startsWith('{') ? 5 : 1}
 						required={required}
-						type={field.type}
 						onChange={(e) => handleChange(e)}
 						className='block w-full p-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm'
 					/>
@@ -95,7 +134,7 @@ export default function DynamicForm({ spec }) {
 		}
 	};
 
-	const renderFormFields = (properties, requiredParent=null, nested=false) => {
+	const renderFormFields = (properties, requiredParent=null, nested=false, hideOptional=false) => {
 		const isFieldRequired = (fieldName) => {
 			return (requiredParent||spec?.connectionSpecification)?.required?.includes(fieldName);
 		};
@@ -117,9 +156,9 @@ export default function DynamicForm({ spec }) {
 				return null;
 			})}
 			{!nested && <div className='space-y-4'>
-				<button type='button' onClick={handleToggleOptionalFields} className='text-indigo-600 hover:text-indigo-900'>
+				{!hideOptional && <button type='button' onClick={handleToggleOptionalFields} className='mt-4 mb-2 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
 					{showOptional ? 'Hide Optional Fields' : 'Show Optional Fields'}
-				</button>
+				</button>}
 				{showOptional && Object.keys(properties).map((key) => {
 					const field = properties[key];
 					if (!field.const //filter out consts
