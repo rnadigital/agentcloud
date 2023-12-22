5aef81f53dd7860956b0fe03749f6d8c6720feee
Thomas Lynch <thomas@rna.digital>
2023-12-22 16:49:29 +1100

    eslinbt fix build and udpate install script to include creating destination, beter printing, colored logo

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-echo """
-            ▓▓▓▓▓▓ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-           ▓▓▓▓▓▓ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-          ▓▓▓▓▓▓ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-          ▓▓▓▓▓  ▒▒▒▒       ▒▒▒
-         ▓▓▓▓▓▓ ▒▒▒▒       ▒▒▒
-        ▓▓▓▓▓▓ ▒▒▒▒       ▒▒▒▒
-        ▓▓▓▓▓  ▒▒▒▒      ▒▒▒▒
-       ▓▓▓▓▓▓ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-      ▓▓▓▓▓▓ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒           ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒▒ ▒▒▒▒▒▒
-     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒           ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒▒  ▒▒▒▒▒▒▒
-    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒           ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒    ▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒
-   ▓▓▓▓▓▓          ▒▒▒▒▒▒▒ ▒▒▒▒▒▒           ▒▒▒▒▒▒▒           ▒▒▒▒▒▒     ▒▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒▒ ▒▒▒▒▒▒ ▒▒▒▒▒
-   ▓▓▓▓▓  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒           ▒▒▒▒▒▒           ▒▒▒▒▒▒     ▒▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒▒  ▒▒▒▒▒▒  ▒▒▒▒▒
-  ▓▓▓▓▓▓ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒           ▒▒▒▒▒▒           ▒▒▒▒▒▒      ▒▒▒▒▒▒ ▒▒▒▒▒▒    ▒▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒
- ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ 
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+BIG_LOGO="""
+\033[34m            ▓▓▓▓▓▓\033[97m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+\033[34m           ▓▓▓▓▓▓\033[97m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+\033[34m          ▓▓▓▓▓▓\033[97m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+\033[34m          ▓▓▓▓▓ \033[97m ▒▒▒▒       ▒▒▒
+\033[34m         ▓▓▓▓▓▓\033[97m ▒▒▒▒       ▒▒▒
+\033[34m        ▓▓▓▓▓▓\033[97m ▒▒▒▒       ▒▒▒▒
+\033[34m        ▓▓▓▓▓ \033[97m ▒▒▒▒      ▒▒▒▒
+\033[34m       ▓▓▓▓▓▓\033[97m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+\033[34m      ▓▓▓▓▓▓\033[97m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+\033[34m     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓\033[97m ▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒           ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒▒ ▒▒▒▒▒▒
+\033[34m     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓\033[97m ▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒           ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒▒  ▒▒▒▒▒▒▒
+\033[34m    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \033[97m ▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒           ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒    ▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒
+\033[34m   ▓▓▓▓▓▓         \033[97m ▒▒▒▒▒▒▒ ▒▒▒▒▒▒           ▒▒▒▒▒▒▒           ▒▒▒▒▒▒     ▒▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒▒ ▒▒▒▒▒▒ ▒▒▒▒▒
+\033[34m   ▓▓▓▓▓ \033[97m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒           ▒▒▒▒▒▒           ▒▒▒▒▒▒     ▒▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒▒  ▒▒▒▒▒▒  ▒▒▒▒▒
+\033[34m  ▓▓▓▓▓▓\033[97m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒           ▒▒▒▒▒▒           ▒▒▒▒▒▒      ▒▒▒▒▒▒ ▒▒▒▒▒▒    ▒▒▒▒▒▒  ▒▒▒▒▒▒    ▒▒▒▒▒
+\033[34m ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓\033[97m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+\033[34m▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \033[97m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ 
+\033[34m▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ \033[97m ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\033[0m
 """
+
+echo -e "$BIG_LOGO"
 
 # get git hash of current repo for footer
 export SHORT_COMMIT_HASH=$(git rev-parse --short HEAD)
@@ -35,6 +37,19 @@ export GCS_BUCKET_NAME
 read -p "Enter your OpenAI API key: " OPENAI_API_KEY
 export OPENAI_API_KEY
 
+clear
+echo -e "$BIG_LOGO"
+echo "=> Starting rabbitmq, qdrant and vector_db_proxy"
+
+# startup rqbbitmq, qdrant, and vector proxy in advance
+docker-compose up rabbitmq -d
+docker-compose up qdrant -d
+# docker-compose up vector_db_proxy -d
+
+clear
+echo -e "$BIG_LOGO"
+echo "=> Starting airbyte"
+
 # clone and install airbyte
 if [ ! -d "airbyte" ] ; then
 	git clone --depth=1 https://github.com/airbytehq/airbyte.git
@@ -47,7 +62,12 @@ cd ..
 INSTANCE_CONFIGURATION=`curl 'http://localhost:8000/api/v1/instance_configuration/setup' -X POST -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Referer: http://localhost:8000/setup' -H 'content-type: application/json' -H 'x-airbyte-analytic-source: webapp' -H 'Origin: http://localhost:8000' -H 'Authorization: Basic YWlyYnl0ZTpwYXNzd29yZA==' -H 'Connection: keep-alive' \
 	--data-raw '{"email":"localhost@localhost.localdomain","anonymousDataCollection":false,"securityCheck":"succeeded","organizationName":"localhost","initialSetupComplete":true,"displaySetupWizard":false}'`
 export AIRBYTE_ADMIN_WORKSPACE_ID=`echo $INSTANCE_CONFIGURATION | jq -r '.defaultWorkspaceId'`
-# TODO: setup qdrand destination and set AIRBYTE_ADMIN_DESTINATION_ID
-export AIRBYTE_ADMIN_DESTINATION_ID=......
+CREATED_DESTINATION=`curl 'http://localhost:8000/api/v1/destinations/create' --compressed -X POST -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0' -H 'Accept: */*' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Referer: http://localhost:8000/workspaces/7b1abeef-4c09-4cad-b23d-539bc236c597/destination/new-destination/e06ad785-ad6f-4647-b2e8-3027a5c59454' -H 'content-type: application/json' -H 'x-airbyte-analytic-source: webapp' -H 'Origin: http://localhost:8000' -H 'DNT: 1' -H 'Authorization: Basic YWlyYnl0ZTpwYXNzd29yZA==' -H 'Connection: keep-alive' \
+             --data-raw '{"name":"RabbitMQ","destinationDefinitionId":"e06ad785-ad6f-4647-b2e8-3027a5c59454","workspaceId":"7b1abeef-4c09-4cad-b23d-539bc236c597","connectionConfiguration":{"routing_key":"key","username":"guest","password":"guest","exchange":"agentcloud","port":5672,"host":"0.0.0.0","ssl":false}}'`
+export AIRBYTE_ADMIN_DESTINATION_ID=`echo $CREATED_DESTINATIO | jq -r '.destinationId'`
 
-#docker-compose up
+clear
+echo -e "$BIG_LOGO"
+echo "=> Starting agentcloud backend..."
+
+docker-compose up
--- webapp/src/pages/[resourceSlug]/team.tsx
+++ webapp/src/pages/[resourceSlug]/team.tsx
@@ -7,9 +7,9 @@ import InviteForm from 'components/InviteForm';
 
 const MemberCard = ({ member }) => {
 	return (
-		<div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
-			<p className="mb-2 font-bold tracking-tight text-gray-900">{member.name}</p>
-			<p className="mb-3 font-normal text-sm text-gray-700">{member.email}</p>
+		<div className='p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md'>
+			<p className='mb-2 font-bold tracking-tight text-gray-900'>{member.name}</p>
+			<p className='mb-3 font-normal text-sm text-gray-700'>{member.email}</p>
 			<span className={`px-3 py-1 text-sm font-semibold text-white rounded-full ${member.emailVerified ? 'bg-green-500' : 'bg-yellow-500'}`}>
 				{member.emailVerified ? 'Active' : 'Pending'}
 			</span>
@@ -50,7 +50,7 @@ export default function Team(props) {
 		</div>
 
 		{/* TODO: a section to show team members properly, and ability to remove from team if emailVerified: false  */}
-		{team && team.length > 0 && <div className="flex flex-wrap gap-4 my-4">
+		{team && team.length > 0 && <div className='flex flex-wrap gap-4 my-4'>
 			{team[0].members.map(member => (
 				<MemberCard key={member._id} member={member} />
 			))}
