3a32c9281e6b811c07edf12fa986c67ce0e5c65a
Thomas Lynch <thomas@rna.digital>
2023-12-22 13:50:11 +1100

    Render cards for team members active/pending

--- webapp/src/pages/[resourceSlug]/team.tsx
+++ webapp/src/pages/[resourceSlug]/team.tsx
@@ -5,6 +5,18 @@ import { useAccountContext } from '../../context/account';
 import { useRouter } from 'next/router';
 import InviteForm from 'components/InviteForm';
 
+const MemberCard = ({ member }) => {
+	return (
+		<div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
+			<p className="mb-2 font-bold tracking-tight text-gray-900">{member.name}</p>
+			<p className="mb-3 font-normal text-sm text-gray-700">{member.email}</p>
+			<span className={`px-3 py-1 text-sm font-semibold text-white rounded-full ${member.emailVerified ? 'bg-green-500' : 'bg-yellow-500'}`}>
+				{member.emailVerified ? 'Active' : 'Pending'}
+			</span>
+		</div>
+	);
+};
+
 export default function Team(props) {
 
 	const [accountContext]: any = useAccountContext();
@@ -38,7 +50,11 @@ export default function Team(props) {
 		</div>
 
 		{/* TODO: a section to show team members properly, and ability to remove from team if emailVerified: false  */}
-		<pre>{JSON.stringify(team, null, 2)}</pre>
+		{team && team.length > 0 && <div className="flex flex-wrap gap-4 my-4">
+			{team[0].members.map(member => (
+				<MemberCard key={member._id} member={member} />
+			))}
+		</div>}
 
 		<div className='border-b pb-2 my-2'>
 			<h3 className='pl-2 font-semibold text-gray-900'>Invite Members:</h3>
