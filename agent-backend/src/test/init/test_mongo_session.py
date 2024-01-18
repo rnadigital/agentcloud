3056e151089af9a562ead3b6727d592352e2dd46
Joshua Kim <joshua.kim@collagis.com.au>
2024-01-18 16:46:18 +1100

    init build

--- agent-backend/src/test/init/test_mongo_session.py
+++ agent-backend/src/test/init/test_mongo_session.py
@@ -1,17 +1,11 @@
+
+import pytest
+
 from init.mongo_session import start_mongo_session
-from init.env_variables import SOCKET_URL, BASE_PATH, AGENT_BACKEND_SOCKET_TOKEN
-from socketio.simple_client import SimpleClient
-from socketio.exceptions import DisconnectedError
-import json
-from build.build_group import ChatBuilder
-from messaging.send_message_to_socket import send
-import requests
-from models.canopy_server import ChatRequest
-from uuid import uuid4
-from autogen.token_count_utils import count_token
-from models.sockets import SocketMessage, SocketEvents, Message
 
 
+
+@pytest.mark.require_docker
 class TestMongoSession:
     def test_mongo_session(self):
         mongo_client = start_mongo_session()
