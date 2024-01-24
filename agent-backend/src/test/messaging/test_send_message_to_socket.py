64b26f864666f46c0650ed906eab47e822958be8
Joshua Kim <joshua.kim@collagis.com.au>
2024-01-24 03:31:41 +0000

    Extend send to logging

--- agent-backend/src/test/messaging/test_send_message_to_socket.py
+++ agent-backend/src/test/messaging/test_send_message_to_socket.py
@@ -2,7 +2,7 @@ import pytest
 from unittest.mock import MagicMock, patch
 from models.sockets import SocketMessage, SocketEvents
 from socketio import SimpleClient
-import messaging.send_message_to_socket as sms
+from messaging import send_message_to_socket as sms
 from pydantic import ValidationError
 
 class TestSend:
@@ -19,9 +19,9 @@ class TestSend:
         return MagicMock(spec=SocketMessage)
 
     def test_send_with_invalid_socket_or_logging(self, mock_client, mock_event, mock_message):
-        with pytest.raises(AssertionError) as excinfo:
-            sms.send(mock_client, mock_event, mock_message, socket_logging="invalid_value")
-        assert "socket_or_logging should be ['socket', 'logging', 'both']" in str(excinfo.value)
+        with pytest.raises((ValidationError, AssertionError, TypeError)) as excinfo:
+            sms.send(mock_client, mock_event, mock_message, socket_or_logging="invalid_value")
+        
 
 
     def test_send_socket_behavior_none_inputs(self):
