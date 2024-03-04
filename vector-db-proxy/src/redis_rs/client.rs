e99f7206efe627e27e7aeb8172c56a1e25cea323
ragy abraham <ragy@rna.digital>
2024-03-04 14:16:22 +1100

    adding prinitng to debug redis address

--- vector-db-proxy/src/redis_rs/client.rs
+++ vector-db-proxy/src/redis_rs/client.rs
@@ -13,8 +13,9 @@ impl RedisConnection {
     pub async fn new(pool_size: Option<u32>) -> RedisResult<Self> {
         let global_data = GLOBAL_DATA.read().await;
         let pool_size = pool_size.unwrap_or(10); // Setting a default value for the pool size
-
-        let redis_connection_manager = RedisConnectionManager::new(format!("redis://{}:{}", global_data.redis_host, global_data.redis_port))?;
+        let redis_address = format!("redis://{}:{}", global_data.redis_host, global_data.redis_port);
+        println!("Redis Address: {redis_address}");
+        let redis_connection_manager = RedisConnectionManager::new(redis_address)?;
 
         let connection_pool = Pool::builder().max_size(pool_size).build(redis_connection_manager).unwrap();
         Ok(RedisConnection { connection_pool })
