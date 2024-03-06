99a8fb87063ac9a19865265f0edb1f318bd4e7bb
pebeid <48051935+pebeid@users.noreply.github.com>
2024-03-06 12:53:45 +1100

    reimplemented sockets and broke build crew into multiple methods

--- agent-backend/src/build/build_crew.py
+++ agent-backend/src/build/build_crew.py
@@ -4,10 +4,12 @@ from bson import ObjectId
 from typing import  Set
 
 from crewai import Agent, Task, Crew
+from socketio.exceptions import ConnectionError as ConnError
+from socketio import SimpleClient
 
 import models.mongo
 from utils.model_helper import keyset
-from init.env_variables import QDRANT_HOST
+from init.env_variables import AGENT_BACKEND_SOCKET_TOKEN, QDRANT_HOST, SOCKET_URL
 from typing import Dict
 from langchain_openai.chat_models import ChatOpenAI, AzureChatOpenAI
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
@@ -62,8 +64,7 @@ class CrewAIBuilder:
                 results[k] = v
         return results
 
-    def build_crew(self):
-        # 1. Build llm/embedding model from Model + Credentials
+    def build_models_with_credentials(self):
         for key, model in self.models_models.items():
             credential = self.match_key(self.credentials_models, key)
             if credential:
@@ -92,26 +93,26 @@ class CrewAIBuilder:
                         self.crew_models[key] = FastEmbedEmbeddings(
                             **model.model_dump(exclude_none=True, exclude_unset=True))
 
-        # 2. Build Crew-Tool from Tool + llm/embedding (#1) + Model (TBD) + Datasource (optional)
+    def build_tools_and_their_datasources(self):
         for key, tool in self.tools_models.items():
-            datasource = self.match_key(self.datasources_models, key)
-            if datasource:
-                embedding_model = self.match_key(self.models_models, key)
-                if model:
-                    tool_factory = RagToolFactory()
-                    collection = str(datasource.id)
-                    tool_factory.init(
-                        Qdrant(
-                            QdrantClient(QDRANT_HOST),
-                            collection_name=collection,
-                            embeddings=FastEmbedEmbeddings(
-                                model_name="BAAI/bge-small-en")),
-                        FastEmbedEmbeddings(
-                            model_name="BAAI/bge-small-en")
-                    )
-                    self.crew_tools[key] = tool_factory.generate_langchain_tool(tool.name, tool.description)
-
-        # 3. Build Crew-Agent from Agent + llm/embedding (#1) + Crew-Tool (#2)
+                    datasource = self.match_key(self.datasources_models, key)
+                    if datasource:
+                        embedding_model = self.match_key(self.models_models, key)
+                        if embedding_model:
+                            tool_factory = RagToolFactory()
+                            collection = str(datasource.id)
+                            tool_factory.init(
+                                Qdrant(
+                                    QdrantClient(QDRANT_HOST),
+                                    collection_name=collection,
+                                    embeddings=FastEmbedEmbeddings(
+                                        model_name="BAAI/bge-small-en")),
+                                FastEmbedEmbeddings(
+                                    model_name="BAAI/bge-small-en")
+                            )
+                            self.crew_tools[key] = tool_factory.generate_langchain_tool(tool.name, tool.description)
+    
+    def build_agents(self):
         for key, agent in self.agents_models.items():
             model_obj = self.match_key(self.crew_models, key, exact=True)
             agent_tools_objs = self.search_subordinate_keys(self.crew_tools, key)
@@ -123,7 +124,7 @@ class CrewAIBuilder:
                 llm=model_obj, tools=agent_tools_objs.values()
             )
 
-        # 4. Build Crew-Task from Task + Crew-Agent (#3) + Crew-Tool (#2)
+    def build_tasks(self):
         for key, task in self.tasks_models.items():
             agent_obj = self.match_key(self.crew_agents, keyset(task.agentId), exact=True)
             task_tools_objs = self.search_subordinate_keys(self.crew_tools, key)
@@ -131,6 +132,19 @@ class CrewAIBuilder:
                 **task.model_dump(exclude_none=True, exclude_unset=True, exclude=["id"]),
                 agent=agent_obj, tools=task_tools_objs.values()
             )
+
+    def build_crew(self):
+        # 1. Build llm/embedding model from Model + Credentials
+        self.build_models_with_credentials()
+
+        # 2. Build Crew-Tool from Tool + llm/embedding (#1) + Model (TBD) + Datasource (optional)
+        self.build_tools_and_their_datasources()    
+
+        # 3. Build Crew-Agent from Agent + llm/embedding (#1) + Crew-Tool (#2)
+        self.build_agents()
+
+        # 4. Build Crew-Task from Task + Crew-Agent (#3) + Crew-Tool (#2)
+        self.build_tasks()
 
         # 5. Build Crew-Crew from Crew + Crew-Task (#4) + Crew-Agent (#3)
         self.crew = Crew(
@@ -141,6 +155,18 @@ class CrewAIBuilder:
                 step_callback=self.send_it
         )
         pprint(self.crew.model_dump())
+
+    def init_socket(self):
+        try:
+            # Initialize the socket client and connect
+            self.socket = SimpleClient()
+            custom_headers = {"x-agent-backend-socket-token": AGENT_BACKEND_SOCKET_TOKEN}
+            print(f"Socker URL:   {SOCKET_URL}")
+            self.socket.connect(url=SOCKET_URL, headers=custom_headers)
+            self.socket.emit("join_room", f"_{self.session_id}")
+        except ConnError as ce:
+            logging.error(f"Connection error occurred: {ce}")
+            raise
 
     def send_it(self, message):
         try:
