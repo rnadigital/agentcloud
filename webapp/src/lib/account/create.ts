702a9882f109d18e4473d80bb2bc2cc9d853c867
Thomas Lynch <thomas@rna.digital>
2024-03-14 12:04:38 +1100

    Add TEAM_ADMIN and ORG_ADMIN instead of removed _OWNER roles in crete account lib

--- webapp/src/lib/account/create.ts
+++ webapp/src/lib/account/create.ts
@@ -30,7 +30,7 @@ export default async function createAccount(email: string, name: string, passwor
 		members: [newAccountId],
 		dateCreated: new Date(),
 		permissions: {
-			[newAccountId.toString()]: new Binary(new Permission(Roles.ORG_OWNER.base64).array),
+			[newAccountId.toString()]: new Binary(new Permission(Roles.ORG_ADMIN.base64).array),
 		},
 	});
 	const addedTeam = await addTeam({
@@ -40,7 +40,7 @@ export default async function createAccount(email: string, name: string, passwor
 		members: [newAccountId],
 		dateCreated: new Date(),
 		permissions: {
-			[newAccountId.toString()]: new Binary(new Permission(Roles.TEAM_OWNER.base64).array),
+			[newAccountId.toString()]: new Binary(new Permission(Roles.TEAM_ADMIN.base64).array),
 		},
 	});
 	const orgId = addedOrg.insertedId;
