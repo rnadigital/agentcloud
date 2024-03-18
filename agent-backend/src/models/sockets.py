4f698f3a4df3abbf3f9b8d91310f33b2b78072e7
Thomas Lynch <thomas@rna.digital>
2024-03-18 16:22:39 +1100

    Avoid pydantic

--- agent-backend/src/models/sockets.py
+++ agent-backend/src/models/sockets.py
@@ -32,7 +32,7 @@ class Message(BaseModel):
     first: Optional[bool] = False
     single: Optional[bool] = False
     type: Optional[MessageType] = MessageType.TEXT.value
-    displayType: Optional[MessageDisplayType] = None
+    displayType: Optional[str] = MessageDisplayType.BUBBLE.value
     timestamp: Optional[float] = datetime.now().timestamp() * 1000
 
 
