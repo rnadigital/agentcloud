4f55ad8bd3771a001b1ed8ee1e635053a9e79269
Thomas Lynch <thomas@rna.digital>
2024-03-19 15:33:47 +1100

    eslint fix

--- webapp/src/components/AgentForm.tsx
+++ webapp/src/components/AgentForm.tsx
@@ -337,14 +337,14 @@ export default function AgentForm({ agent = {}, models = [], tools=[], groups=[]
 									</div>
 								))}
 							</div>
-								<button 
-									className={`w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${compact ? 'w-full' : ''}`}
-									onClick={(e) => {
+							<button 
+								className={`w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${compact ? 'w-full' : ''}`}
+								onClick={(e) => {
 									e.preventDefault();
 									setModalOpen('tool');
 								}}>
 									+ New Tool
-								</button>
+							</button>
 
 						</div>
 					</div>
