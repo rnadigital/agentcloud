78a65598929b1cea352d64ced455a724d9c0981b
Thomas Lynch <thomas@rna.digital>
2024-03-20 14:43:55 +1100

    Update subscriptionmodal to be customisable to pop it up for different limits

--- webapp/src/components/SubscriptionModal.tsx
+++ webapp/src/components/SubscriptionModal.tsx
@@ -6,7 +6,7 @@ import { Fragment, useState } from 'react';
 import * as API from '../api';
 import { useAccountContext } from '../context/account';
 
-export default function SubscriptionModal({ open, setOpen }) {
+export default function SubscriptionModal({ open, setOpen, title = 'Upgrade Required', text, buttonText = 'Upgrade' }) {
 
 	const [accountContext]: any = useAccountContext();
 	const { csrf } = accountContext as any;
@@ -52,11 +52,11 @@ export default function SubscriptionModal({ open, setOpen }) {
 									</div>
 									<div className='mt-3 text-center sm:mt-5'>
 										<Dialog.Title as='h3' className='text-base font-semibold leading-6 text-gray-900'>
-											Unlock Sessions
+											{title}
 										</Dialog.Title>
 										<div className='mt-2'>
 											<p className='text-sm text-gray-500'>
-												Subscribe to create your unique session today!
+												{text}
 											</p>
 										</div>
 									</div>
@@ -67,7 +67,7 @@ export default function SubscriptionModal({ open, setOpen }) {
 										className='inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
 										onClick={getPaymentLink}
 									>
-										Subscribe
+										{buttonText}
 									</button>
 								</div>
 							</Dialog.Panel>
