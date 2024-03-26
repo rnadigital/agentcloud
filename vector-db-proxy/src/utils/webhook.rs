6851d8ff8dcc6cdc05574fd1fd5b4d56b9f44c8d
Thomas Lynch <thomas@rna.digital>
2024-03-26 13:15:14 +1100

    Add WEBAPP_PORT env and port default 3000 for webapp webhook host

--- vector-db-proxy/src/utils/webhook.rs
+++ vector-db-proxy/src/utils/webhook.rs
@@ -5,7 +5,7 @@ use crate::init::env_variables::GLOBAL_DATA;
 
 pub async fn send_webapp_embed_ready(datasource_id: &str) -> Result<(), anyhow::Error> {
     let global_data = GLOBAL_DATA.read().await;
-    let url = format!("http://{}{}", global_data.webapp_host, "/webhook/embed-successful");
+    let url = format!("http://{}:{}{}", global_data.webapp_host, global_data.webapp_port, "/webhook/embed-successful");
 
     // Prepare the POST request body
     let body = json!({
