2a0291dcd54e6f3b79c27e4b763af4ce777bb99d
Thomas Lynch <thomas@rna.digital>
2024-04-02 11:26:41 +1100

    Add default fill to agentavatar to fix typing issue

--- webapp/src/components/AgentAvatar.tsx
+++ webapp/src/components/AgentAvatar.tsx
@@ -1,5 +1,5 @@
 import Blockies from 'react-blockies';
-export default function AgentAvatar({ agent, fill, size=16 }) {
+export default function AgentAvatar({ agent, fill=false, size=16 }) {
 	return <span className='rounded-full overflow-hidden'>
 		{agent?.icon?.filename
 			? <img className={`object-cover ${fill ? 'w-full h-full' : `h-${size} w-${size}`}`} src={`https://storage.googleapis.com/${process.env.NEXT_PUBLIC_GCS_BUCKET_NAME}/${agent.icon.filename}`} />
