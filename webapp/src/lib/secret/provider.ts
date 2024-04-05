604994084f4cb0c81c3613f71a7358f5a525123f
Thomas Lynch <thomas@rna.digital>
2024-04-05 14:13:48 +1100

    Overhaul secret management to use a factory/provider pattern just like storages, and implement google secrets and local (.env) for now

---
+++ webapp/src/lib/secret/provider.ts
@@ -0,0 +1,15 @@
+export default class SecretProvider {
+
+	async init() {
+
+	}
+
+	async getSecret(key: string, bypassCache: boolean = false) {
+		throw new Error('getSecret method not implemented');
+	}
+
+	// I don't think we need these
+	// async setSecret() { ... }
+	// async deleteSecret() { ... }
+
+}
