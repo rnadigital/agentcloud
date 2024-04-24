246147f3c9368ef190839876e3b64ec219c68155
Thomas Lynch <thomas@rna.digital>
2024-04-24 16:13:18 +1000

    Remove required prop from tool form

--- webapp/src/components/ToolSelector.tsx
+++ webapp/src/components/ToolSelector.tsx
@@ -33,7 +33,6 @@ const ToolSelector = ({ children, tools, toolState, setToolState }) => {
 						value={searchTerm}
 						onChange={handleSearchChange}
 						className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-slate-800 dark:ring-slate-600 dark:text-white'
-						required
 					/>
 				</div>
 			</div>
