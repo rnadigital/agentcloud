bba0c4969a404f35399a6b4ee23f86915d9199d8
Charles John <1017170+charl3sj@users.noreply.github.com>
2024-04-24 14:13:40 +0530

    Use kwargs for SelfQueryRetriever instantiation

--- agent-backend/src/tools/retrievers/self_query.py
+++ agent-backend/src/tools/retrievers/self_query.py
@@ -13,7 +13,10 @@ class SelfQueryRetriever(BaseToolRetriever):
             map(lambda x: AttributeInfo(**x.model_dump()), tool.retriever_config.metadata_field_info))
 
         # TODO: Change the `document_contents` param value from `tool.description` to something else
-        self.retriever = LC_SelfQueryRetriever.from_llm(llm, vector_store, tool.description, metadata_field_info)
+        self.retriever = LC_SelfQueryRetriever.from_llm(llm=llm,
+                                                        vectorstore=vector_store,
+                                                        document_contents=tool.description,
+                                                        metadata_field_info=metadata_field_info)
 
     def run(self, query):
         results = self.retriever.invoke(query)
