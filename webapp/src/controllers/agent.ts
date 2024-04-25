990e143fd87a6435d5ae3004d53df8b9313bfde3
Thomas Lynch <thomas@rna.digital>
2024-04-26 09:38:52 +1000

    Revert "Make function calling model optional in agent form"
    This reverts commit a16c3bbec9e564a48134bc1c02b85b06d7219bcb.

--- webapp/src/controllers/agent.ts
+++ webapp/src/controllers/agent.ts
@@ -117,7 +117,6 @@ export async function addAgentApi(req, res, next) {
 	let validationError = chainValidations(req.body, [
 		{ field: 'name', validation: { notEmpty: true, lengthMin: 2 }},
 		{ field: 'modelId', validation: { notEmpty: true, hasLength: 24 }},
-		// { field: 'functionModelId', validation: { hasLength: 24 }},
 		{ field: 'role', validation: { notEmpty: true, lengthMin: 2 }},
 		{ field: 'goal', validation: { notEmpty: true, lengthMin: 2 }},
 		{ field: 'backstory', validation: { notEmpty: true, lengthMin: 2 }},
@@ -204,7 +203,7 @@ export async function editAgentApi(req, res, next) {
 	let validationError = chainValidations(req.body, [
 		{ field: 'name', validation: { notEmpty: true, lengthMin: 2 }},
 		{ field: 'modelId', validation: { notEmpty: true, hasLength: 24 }},
-		// { field: 'functionModelId', validation: { hasLength: 24 }},
+		{ field: 'functionModelId', validation: { hasLength: 24 }},
 		{ field: 'role', validation: { notEmpty: true, lengthMin: 2 }},
 		{ field: 'goal', validation: { notEmpty: true, lengthMin: 2 }},
 		{ field: 'backstory', validation: { notEmpty: true, lengthMin: 2 }},
