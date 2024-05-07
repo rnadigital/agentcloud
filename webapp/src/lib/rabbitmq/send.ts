d8909c81f38be8360f98b03f724f297c9e524244
ragy abraham <ragy@rna.digital>
2024-05-07 11:48:50 +1000

    added rabbitmq username and password in connection string for webapp

--- webapp/src/lib/rabbitmq/send.ts
+++ webapp/src/lib/rabbitmq/send.ts
@@ -11,9 +11,9 @@ let connection: Connection | null = null
 	, channel: Channel | null = null;
 
 export async function initRabbit() {
-	console.log('rabbitmq_host: %d', process.env.RABBITMQ_HOST);
+	console.log('rabbitmq_host: %s', process.env.RABBITMQ_HOST);
 	console.log('rabbitmq_port: %d', process.env.RABBITMQ_PORT);
-	connection = await connect(`amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`);
+	connection = await connect(`amqp://${process.env.RABBITMQ_USERNAME}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`);
 	channel = await connection.createChannel();
 	await channel.assertExchange('streaming', 'direct', { durable: true });
 }
