afbf0553526234907c1865ae10965fd6b27f78c5
Thomas Lynch <thomas@rna.digital>
2024-05-08 13:51:46 +1000

    Make datasource status indicator more accurate when not complete

--- webapp/src/components/DatasourceStatusIndicator.tsx
+++ webapp/src/components/DatasourceStatusIndicator.tsx
@@ -3,11 +3,12 @@ import ProgressBar from 'components/ProgressBar';
 import { DatasourceStatus, datasourceStatusColors } from 'struct/datasource';
 
 function DatasourceStatusIndicator({ datasource, processingOrEmbedding }) {
-	const finished = ((datasource.recordCount?.success+datasource.recordCount?.failure) < datasource.recordCount?.total);
+	const finished = ((datasource.recordCount?.success+datasource.recordCount?.failure) >= datasource.recordCount?.total);
+	finished && (datasource.status = DatasourceStatus.READY);
 	return (DatasourceStatus.EMBEDDING === datasource.status && !finished)
 		? <ProgressBar total={datasource.recordCount?.total} success={datasource.recordCount?.success} failure={datasource.recordCount?.failure} />
 		: <div className={`max-w-[300px] px-3 py-[2px] text-sm text-white text-center rounded-full capitalize ${(processingOrEmbedding && !finished) ? 'barberpole' : datasourceStatusColors[datasource.status]}`}>
-			{datasource.status || 'Unknown'}{processingOrEmbedding && <ButtonSpinner size={14} className='ms-2 -me-1' />}
+			{datasource.status || 'Unknown'}{!finished && <ButtonSpinner size={14} className='ms-2 -me-1' />}
 		</div>;
 }
 
