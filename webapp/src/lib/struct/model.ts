4f4b253e1ccbc280b600e1c44c817c39b59df7ed
Thomas Lynch <thomas@rna.digital>
2024-05-14 15:42:12 +1000

    Update models list for gpt

--- webapp/src/lib/struct/model.ts
+++ webapp/src/lib/struct/model.ts
@@ -3,7 +3,31 @@
 import { CredentialType } from 'struct/credential';
 
 export const ModelList = {
-	[CredentialType.OPENAI]: ['gpt-4-0125-preview', 'gpt-3.5-turbo-0125', 'gpt-3.5-turbo', 'gpt-4', 'gpt-4-1106-preview', 'text-embedding-3-large', 'text-embedding-3-small', 'text-embedding-ada-002'],
+	[CredentialType.OPENAI]: [
+		'gpt-4o',
+		'gpt-4o-2024-05-13',
+		'gpt-4-turbo',
+		'gpt-4-turbo-2024-04-09',
+		'gpt-4-turbo-preview',
+		'gpt-4-0125-preview',
+		'gpt-4-1106-preview',
+		'gpt-4-vision-preview',
+		'gpt-4-1106-vision-preview',
+		'gpt-4',
+		'gpt-4-0613',
+		'gpt-4-32k',
+		'gpt-4-32k-0613',
+		'gpt-3.5-turbo-0125',
+		'gpt-3.5-turbo',
+		'gpt-3.5-turbo-1106',
+		'gpt-3.5-turbo-instruct',
+		'gpt-3.5-turbo-16k',
+		'gpt-3.5-turbo-0613',
+		'gpt-3.5-turbo-16k-0613',
+		'text-embedding-3-large',
+		'text-embedding-3-small',
+		'text-embedding-ada-002'
+	],
 	[CredentialType.FASTEMBED]: [
 		'fast-bge-small-en',
 		'fast-bge-small-en-v1.5',
