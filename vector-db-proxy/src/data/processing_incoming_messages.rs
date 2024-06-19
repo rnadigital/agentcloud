7b7f494af933861f2060345281c340a4b6733531
ragy abraham <ragy@rna.digital>
2024-06-19 17:11:07 +1000

    added a match statemt to catch error if the text field is not the metadata

--- vector-db-proxy/src/data/processing_incoming_messages.rs
+++ vector-db-proxy/src/data/processing_incoming_messages.rs
@@ -34,42 +34,49 @@ pub async fn process_streaming_messages(
                         let mut field_path = "recordCount.failure";
                         if let Value::Object(data_obj) = message_data {
                             let mut metadata = convert_serde_value_to_hashmap_string(data_obj);
+                            log::debug!("Metadata: {:?}", metadata);
                             if let Some(text_field) = embedding_field {
-                                log::debug!("text field: {}", text_field.as_str());
-                                let text = metadata.remove(text_field.as_str()).unwrap();
-                                metadata.insert("page_content".to_string(), text.to_owned());
-                                let mongo_conn_clone = Arc::clone(&mongo_conn);
-                                let ds_clone = ds_clone.clone();
-                                match embed_payload(
-                                    mongo_conn_clone,
-                                    &metadata,
-                                    &text,
-                                    Some(ds_clone.clone()),
-                                    EmbeddingModels::from(embedding_model_name)).await {
-                                    Ok(point_struct) => {
-                                        match qdrant.upsert_data_point_blocking(point_struct).await {
-                                            Ok(result) => {
-                                                match result {
-                                                    true => {
-                                                        field_path = "recordCount.success";
-                                                        let _ = increment_by_one(&mongodb_connection, &*ds_clone, field_path).await.unwrap();
+                                log::debug!("text field: {}", text_field.as_str().to_lowercase());
+                                match metadata.remove(text_field.to_lowercase().as_str()) {
+                                    Some(t) => {
+                                        metadata.insert("page_content".to_string(), t.to_owned());
+                                        let mongo_conn_clone = Arc::clone(&mongo_conn);
+                                        let ds_clone = ds_clone.clone();
+                                        match embed_payload(
+                                            mongo_conn_clone,
+                                            &metadata,
+                                            &t,
+                                            Some(ds_clone.clone()),
+                                            EmbeddingModels::from(embedding_model_name)).await {
+                                            Ok(point_struct) => {
+                                                match qdrant.upsert_data_point_blocking(point_struct).await {
+                                                    Ok(result) => {
+                                                        match result {
+                                                            true => {
+                                                                field_path = "recordCount.success";
+                                                                increment_by_one(&mongodb_connection, &ds_clone, field_path).await.unwrap();
+                                                            }
+                                                            false => {
+                                                                log::warn!("An error occurred while inserting into vector database");
+                                                                increment_by_one(&mongodb_connection, &ds_clone, field_path).await.unwrap();
+                                                            }
+                                                        }
                                                     }
-                                                    false => {
-                                                        log::warn!("An error occurred while inserting into vector database");
-                                                        let _ = increment_by_one(&mongodb_connection, &*ds_clone, field_path).await.unwrap();
+                                                    Err(e) => {
+                                                        log::warn!("An error occurred while inserting into vector database. Error: {}", e);
+                                                        increment_by_one(&mongodb_connection, &ds_clone, field_path).await.unwrap();
                                                     }
                                                 }
+                                                increment_by_one(&mongodb_connection, &ds_clone, field_path).await.unwrap();
                                             }
                                             Err(e) => {
-                                                log::warn!("An error occurred while inserting into vector database. Error: {}", e);
-                                                let _ = increment_by_one(&mongodb_connection, &*ds_clone, field_path).await.unwrap();
+                                                increment_by_one(&mongodb_connection, &ds_clone, field_path).await.unwrap();
+                                                log::error!("An error occurred while upserting  point structs to Qdrant: {}", e);
                                             }
                                         }
-                                        let _ = increment_by_one(&mongodb_connection, &*ds_clone, field_path).await.unwrap();
                                     }
-                                    Err(e) => {
-                                        let _ = increment_by_one(&mongodb_connection, &*ds_clone, field_path).await.unwrap();
-                                        log::error!("An error occurred while upserting  point structs to Qdrant: {}", e);
+                                    None => {
+                                        log::error!("The text field {} returned None!", text_field)
                                     }
                                 }
                             }
