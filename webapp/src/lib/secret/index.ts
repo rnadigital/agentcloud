6fb629a0e5ef8a054b71504280870da7910b4f41
Thomas Lynch <thomas@rna.digital>
2024-06-26 16:34:41 +1000

    Make secret provider factory default to local

--- webapp/src/lib/secret/index.ts
+++ webapp/src/lib/secret/index.ts
@@ -2,7 +2,7 @@ import GoogleSecretProvider from 'secret/google';
 import LocalSecretProvider from 'secret/local';
 
 export default class SecretProviderFactory {
-	static getSecretProvider(provider: string = '') {
+	static getSecretProvider(provider: string = 'local') {
 		switch ((process.env.NEXT_PUBLIC_SECRET_PROVIDER||provider).toLowerCase()) {
 			case 'google':
 				return GoogleSecretProvider;
