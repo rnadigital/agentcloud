716354ba77ef6c00eb46a9bfcd1ac0314a19527f
Thomas Lynch <thomas@rna.digital>
2024-07-01 16:59:06 +1000

    eslint and add initial parameter form to simple app form

--- webapp/src/components/SimpleAppForm.tsx
+++ webapp/src/components/SimpleAppForm.tsx
@@ -5,11 +5,12 @@ import {
 	HandRaisedIcon,
 	PlayIcon
 } from '@heroicons/react/20/solid';
-import InfoAlert from 'components/InfoAlert';
 import AvatarUploader from 'components/AvatarUploader';
 import CreateDatasourceModal from 'components/CreateDatasourceModal';
 import formatDatasourceOptionLabel from 'components/FormatDatasourceOptionLabel';
+import InfoAlert from 'components/InfoAlert';
 import PageTitleWithNewButton from 'components/PageTitleWithNewButton';
+import ParameterForm from 'components/ParameterForm';
 import { useAccountContext } from 'context/account';
 import { useStepContext } from 'context/stepwrapper';
 import dynamic from 'next/dynamic';
@@ -140,7 +141,6 @@ export default function SimpleAppForm({ datasourceChoices=[], callback, fetchFor
 								type='text'
 								name='name'
 								id='name'
-								defaultValue={name}
 								className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-slate-800 dark:ring-slate-600 dark:text-white'
 							/>
 						</div>
@@ -162,17 +162,21 @@ export default function SimpleAppForm({ datasourceChoices=[], callback, fetchFor
 							</div>
 						</div>
 
-						{/*<ParameterForm 
-							readonly={isBuiltin} 
-							parameters={environmentVariables} 
-							setParameters={setEnvironmentVariables} 
-							title='Environment Variables' 
-							disableTypes={true} 
-							hideRequired={true} 
-							namePattern='[A-Z][A-Z0-9_]*'
-							namePlaceholder='Key must be uppercase seperated by underscores'
-							descriptionPlaceholder='Value'
-						/>*/}
+
+						<div className='sm:col-span-12'>
+							<ParameterForm 
+								readonly={false} 
+								parameters={[{}]} 
+								setParameters={() => {}} 
+								title='Conversation Starters' 
+								disableTypes={true} 
+								disableDescription={true}
+								hideRequired={true} 
+								namePattern='[A-Z][A-Z0-9_]*'
+								namePlaceholder='Key must be uppercase seperated by underscores'
+								descriptionPlaceholder='Value'
+							/>
+						</div>
 
 						<div className='sm:col-span-12'>
 							<label htmlFor='type' className='block text-sm font-medium leading-6 text-gray-900 dark:text-slate-400'>
