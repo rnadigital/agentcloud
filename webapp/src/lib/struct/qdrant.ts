3e81392fd324fa2d627e3c814d9146cbe32005b4
Thomas Lynch <thomas@rna.digital>
2024-07-10 11:41:21 +1000

    Add vector proxy collection-info function and struct for return type

---
+++ webapp/src/lib/struct/qdrant.ts
@@ -0,0 +1,6 @@
+export type CollectionInfo = {
+	status: number;
+	indexed_vectors_count?: number;
+	segments_count: number;
+	points_count?: number;
+};
--- webapp/src/lib/vectordb/proxy.ts
+++ webapp/src/lib/vectordb/proxy.ts
@@ -3,25 +3,33 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 import { IdOrStr } from 'db/index';
+import { CollectionInfo } from 'struct/qdrant';
 
 class VectorDBProxy {
 
 	// Method to create a collection in Qdrant
-	static async createCollectionInQdrant(collectionId: IdOrStr) {
+	static async createCollectionInQdrant(collectionId: IdOrStr): Promise<Response> {
 		return fetch(`${process.env.VECTOR_APP_URL}/api/v1/check-collection-exists/${collectionId}`, {
 			method: 'POST',
 		});
 	}
 
 	// Method to get collection details
-	static async getCollectionFromQdrant(collectionId: IdOrStr) {
+	static async getCollectionFromQdrant(collectionId: IdOrStr): Promise<Response> {
 		return fetch(`${process.env.VECTOR_APP_URL}/api/v1/collection/${collectionId}`, {
 			method: 'GET',
 		});
 	}
 
+	// Method to get collection info
+	static async getCollectionInfoFromQdrant(collectionId: IdOrStr): Promise<CollectionInfo> {
+		return fetch(`${process.env.VECTOR_APP_URL}/api/v1/collection-info/${collectionId}`, {
+			method: 'GET',
+		}).then(res => res.json());
+	}
+
 	// Method to delete a collection from Qdrant
-	static async deleteCollectionFromQdrant(collectionId: IdOrStr) {
+	static async deleteCollectionFromQdrant(collectionId: IdOrStr): Promise<Response> {
 		return fetch(`${process.env.VECTOR_APP_URL}/api/v1/collection/${collectionId}`, {
 			method: 'DELETE',
 		});
