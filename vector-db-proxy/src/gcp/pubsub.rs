29aaeef237f8ba4eed5a0e51f8364c0b1419789d
ragy abraham <ragy@rna.digital>
2024-07-22 16:08:52 +1000

    explicitly using the JSON google creds

--- vector-db-proxy/src/gcp/pubsub.rs
+++ vector-db-proxy/src/gcp/pubsub.rs
@@ -24,50 +24,42 @@ pub async fn subscribe_to_topic(
     let cred_file = try_auth_to_google().await.unwrap();
     let client_config = ClientConfig::default().with_credentials(cred_file).await.unwrap();
     
-    match ClientConfig::default().with_auth().await {
-        Ok(client_config) => {
-            match Client::new(client_config).await {
-                Ok(client) => {
-                    // Get the topic to subscribe to.
-                    if !client.topic(
-                        connection_details.topic
-                            .as_str()
-                    ).exists(None).await? {
-                        println!("Topic: {} does not exist. Creating it now...", connection_details.topic.as_str());
-                        client.topic(connection_details.topic
-                            .as_str()).create(Some(TopicConfig::default()), None).await?;
-                        println!("Topic: {} created successfully", connection_details.topic.as_str());
-                    }
-                    let topic = client.topic(connection_details.topic.as_str());
-                    // Create subscription
-                    let subscription = client
-                        .subscription(
-                            connection_details.topic
-                                .as_str()
-                        );
-                    if !subscription.exists(None).await? {
-                        println!("Subscription: {} does not exist. Creating it now...", connection_details.subscription.as_str());
-                        subscription.create(
-                            topic
-                                .fully_qualified_name(),
-                            SubscriptionConfig::default(),
-                            None)
-                            .await?;
-                        println!("Subscription: {} created successfully", connection_details.subscription.as_str());
-                    }
-                    println!("Listening to messages on subscription: {}", subscription.fully_qualified_name());
-
-                    let stream = subscription
-                        .subscribe(None)
-                        .await?;
-                    Ok(stream)
-                },
-                Err(e) => Err(anyhow!("An error occurred building PubSub client. {}", e))
+    match Client::new(client_config).await {
+        Ok(client) => {
+            // Get the topic to subscribe to.
+            if !client.topic(
+                connection_details.topic
+                    .as_str()
+            ).exists(None).await? {
+                println!("Topic: {} does not exist. Creating it now...", connection_details.topic.as_str());
+                client.topic(connection_details.topic
+                    .as_str()).create(Some(TopicConfig::default()), None).await?;
+                println!("Topic: {} created successfully", connection_details.topic.as_str());
             }
+            let topic = client.topic(connection_details.topic.as_str());
+            // Create subscription
+            let subscription = client
+                .subscription(
+                    connection_details.topic
+                        .as_str()
+                );
+            if !subscription.exists(None).await? {
+                println!("Subscription: {} does not exist. Creating it now...", connection_details.subscription.as_str());
+                subscription.create(
+                    topic
+                        .fully_qualified_name(),
+                    SubscriptionConfig::default(),
+                    None)
+                    .await?;
+                println!("Subscription: {} created successfully", connection_details.subscription.as_str());
+            }
+            println!("Listening to messages on subscription: {}", subscription.fully_qualified_name());
+
+            let stream = subscription
+                .subscribe(None)
+                .await?;
+            Ok(stream)
         },
-        Err(e) => {
-            Err(anyhow!("An error occurred while build the client config for PubSub. Error: \
-                {}", e))
-        }
+        Err(e) => Err(anyhow!("An error occurred building PubSub client. {}", e))
     }
 }
