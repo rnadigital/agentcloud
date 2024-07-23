2f6d24cf478293e9f24590a72fc3d193a1d0ee77
Thomas Lynch <thomas@rna.digital>
2024-07-23 14:41:58 +1000

    Correct links for "remembered your password?" to go to login instead of register

--- webapp/src/pages/changepassword.tsx
+++ webapp/src/pages/changepassword.tsx
@@ -106,7 +106,7 @@ export default function ChangePassword() {
 					<p className='mt-10 text-center text-sm text-gray-500'>
 						Remembered your password?{' '}
 						<Link
-							href='/register'
+							href='/login'
 							className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
 						>
 							Sign in
