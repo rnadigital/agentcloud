90989cf9e79b676335db538ae3cbdbbca35b811a
Thomas Lynch <thomas@rna.digital>
2024-07-25 13:42:34 +1000

    Make the "public" chip on app cards click to copy link Add icon, toltip and cursor styling for affordance

--- webapp/src/components/AppCard.tsx
+++ webapp/src/components/AppCard.tsx
@@ -2,7 +2,7 @@
 import * as API from '@api';
 import { Menu, Transition } from '@headlessui/react';
 import { PlayIcon } from '@heroicons/react/20/solid';
-import { EllipsisHorizontalIcon } from '@heroicons/react/20/solid';
+import { EllipsisHorizontalIcon, LinkIcon } from '@heroicons/react/20/solid';
 import AgentAvatar from 'components/AgentAvatar';
 import classNames from 'components/ClassNames';
 import { useAccountContext } from 'context/account';
@@ -18,6 +18,8 @@ export default function AppCard({ app, startSession, fetchFormData }) {
 	const { csrf } = accountContext as any;
 	const router = useRouter();
 	const { resourceSlug } = router.query;
+	const origin = typeof location !== 'undefined' ? location.origin : '';
+
 	async function deleteApp(appId) {
 		await API.deleteApp(
 			{
@@ -34,77 +36,95 @@ export default function AppCard({ app, startSession, fetchFormData }) {
 		fetchFormData && fetchFormData();
 	}
 	return (
-		<div className='w-full max-w-sm rounded-xl overflow-hidden bg-white border dark:bg-slate-800 px-6 py-4 flex flex-col space-between min-h-50'>
-			<a className='h-full' href={`/${resourceSlug}/app/${app._id}/edit`}>
-				<span className='flex justify-between'>
-					<span className='space-x-2'>
-						<span
-							className={`h-6 px-2 py-[0.5px] border text-sm rounded-lg ${
-								app.type === AppType.CHAT
-									? 'bg-blue-100 text-blue-800 border-blue-300'
-									: 'bg-green-100 text-green-800 border-green-300'
-							}`}
-						>
-							{app.type === AppType.CHAT ? 'Chat' : 'Process'}
-						</span>
-						{app?.sharingConfig?.mode === SharingMode.PUBLIC && (
-							<span className='h-6 px-2 py-[0.5px] border text-sm rounded-lg bg-yellow-100 text-yellow-800 border-yellow-300'>
-								Public
-							</span>
-						)}
+		<div className='w-full max-w-sm rounded-xl bg-white border dark:bg-slate-800 px-6 py-4 flex flex-col space-between min-h-50'>
+			<span className='flex justify-between'>
+				<span className='space-x-2'>
+					<span
+						className={`h-6 px-2 py-[0.5px] border text-sm rounded-lg ${
+							app.type === AppType.CHAT
+								? 'bg-blue-100 text-blue-800 border-blue-300'
+								: 'bg-green-100 text-green-800 border-green-300'
+						}`}
+					>
+						{app.type === AppType.CHAT ? 'Chat' : 'Process'}
 					</span>
-					<Menu as='div' className=''>
-						<Menu.Button className='block p-2.5 text-gray-400 hover:text-gray-500 hover:text-white hover:bg-gray-700 rounded'>
-							<span className='sr-only'>Open options</span>
-							<EllipsisHorizontalIcon className='h-5 w-5' aria-hidden='true' />
-						</Menu.Button>
-						<Transition
-							as={Fragment}
-							enter='transition ease-out duration-100'
-							enterFrom='transform opacity-0 scale-95'
-							enterTo='transform opacity-100 scale-100'
-							leave='transition ease-in duration-75'
-							leaveFrom='transform opacity-100 scale-100'
-							leaveTo='transform opacity-0 scale-95'
-						>
-							<Menu.Items className='absolute z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white dark:bg-slate-800 py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none'>
-								{/*<Menu.Item>
-									{({ active }) => (
-										<a
-											href={`/${resourceSlug}/app/${app._id}/edit`}
-											className={classNames(
-												active ? 'bg-gray-50 dark:bg-slate-700' : '',
-												'block px-3 py-1 text-sm leading-6 text-gray-900 dark:text-white'
-											)}
-										>
-											Edit
-										</a>
-									)}
-								</Menu.Item>*/}
-								<Menu.Item>
-									{({ active }) => (
-										<button
-											onClick={e => {
-												e.stopPropagation();
-												e.preventDefault();
-												deleteApp(app._id);
-											}}
-											className={classNames(
-												active ? 'bg-gray-50 dark:bg-slate-700' : '',
-												'block px-3 py-1 text-sm leading-6 text-red-600 w-full text-left'
-											)}
-										>
-											Delete
-										</button>
-									)}
-								</Menu.Item>
-							</Menu.Items>
-						</Transition>
-					</Menu>
+					{app?.sharingConfig?.mode === SharingMode.PUBLIC && (
+						<span className='tooltip z-100'>
+							<span className='tooltiptext text-sm capitalize !w-[80px] !-ml-[40px] whitespace-pre'>
+								Copy Link
+							</span>
+							<span
+								className='cursor-pointer h-6 px-2 py-[0.5px] border text-sm rounded-lg bg-yellow-100 text-yellow-800 border-yellow-300'
+								onClick={async () => {
+									try {
+										//TODO: refactor the construction of this public app URL to some unified place
+										await navigator.clipboard.writeText(
+											`${origin}/s/${resourceSlug}/app/${app._id}`
+										);
+										toast.success('Link copied');
+									} catch {
+										/* ignored for now */
+									}
+								}}
+							>
+								Public <LinkIcon className='h-3 w-3 inline' />
+							</span>
+						</span>
+					)}
 				</span>
-				<div className='flex items-center justify-center p-4'>
-					<AgentAvatar agent={app} size={20} />
-				</div>
+				<Menu as='div' className=''>
+					<Menu.Button className='block p-2.5 text-gray-400 hover:text-gray-500 hover:text-white hover:bg-gray-700 rounded'>
+						<span className='sr-only'>Open options</span>
+						<EllipsisHorizontalIcon className='h-5 w-5' aria-hidden='true' />
+					</Menu.Button>
+					<Transition
+						as={Fragment}
+						enter='transition ease-out duration-100'
+						enterFrom='transform opacity-0 scale-95'
+						enterTo='transform opacity-100 scale-100'
+						leave='transition ease-in duration-75'
+						leaveFrom='transform opacity-100 scale-100'
+						leaveTo='transform opacity-0 scale-95'
+					>
+						<Menu.Items className='absolute z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white dark:bg-slate-800 py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none'>
+							{/*<Menu.Item>
+								{({ active }) => (
+									<a
+										href={`/${resourceSlug}/app/${app._id}/edit`}
+										className={classNames(
+											active ? 'bg-gray-50 dark:bg-slate-700' : '',
+											'block px-3 py-1 text-sm leading-6 text-gray-900 dark:text-white'
+										)}
+									>
+										Edit
+									</a>
+								)}
+							</Menu.Item>*/}
+							<Menu.Item>
+								{({ active }) => (
+									<button
+										onClick={e => {
+											e.stopPropagation();
+											e.preventDefault();
+											deleteApp(app._id);
+										}}
+										className={classNames(
+											active ? 'bg-gray-50 dark:bg-slate-700' : '',
+											'block px-3 py-1 text-sm leading-6 text-red-600 w-full text-left'
+										)}
+									>
+										Delete
+									</button>
+								)}
+							</Menu.Item>
+						</Menu.Items>
+					</Transition>
+				</Menu>
+			</span>
+			<div className='flex items-center justify-center p-4'>
+				<AgentAvatar agent={app} size={20} />
+			</div>
+			<a className='h-full' href={`/${resourceSlug}/app/${app._id}/edit`}>
 				<div>
 					<div className='font-bold text-xl mb-2'>{name}</div>
 					<p className={'text-gray-700 text-base max-h-20 overflow-hidden dark:text-white'}>
