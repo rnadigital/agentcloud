b924c4871a7b6ba61f210950633035bf66b08de3
Ian Christopher B. de Jesus <3683356+iandjx@users.noreply.github.com>
2024-08-01 17:20:35 +0800

    Add session on redirect and automatically send message

--- webapp/ecosystem.config.js
+++ webapp/ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
     script: "npm run dev",
     watch: true,
     // watch_delay: 1000,
-    ignore_watch : ["node_modules", "src/components", "src/pages", "src/context", "\\.next", "\\.dist", "tsconfig.tsbuildinfo"],
+    ignore_watch: ["node_modules", "src/components", "src/pages", "src/context", "\\.next", "\\.dist", "tsconfig.tsbuildinfo",".DS_Store" 
+    ],
     "env": {
       "DEBUG": "webapp*,-webapp:storage*,-webapp:middleware:auth:*",
     },
