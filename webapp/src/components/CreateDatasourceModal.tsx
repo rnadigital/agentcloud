8d4c8730dd235c91aa9f464df57e6804ca06e4a0
Ian Christopher B. de Jesus <3683356+iandjx@users.noreply.github.com>
2024-08-06 08:52:41 +0800

    Add dark theme styling to pages and modals

--- webapp/src/components/CreateDatasourceModal.tsx
+++ webapp/src/components/CreateDatasourceModal.tsx
@@ -1,5 +1,5 @@
 import * as API from '@api';
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
 import CreateDatasourceForm from 'components/CreateDatasourceForm'; // Assuming this component is similar to ModelForm but for datasources
 import { useAccountContext } from 'context/account';
 import FormContext from 'context/connectorform';
@@ -23,9 +23,9 @@ export default function CreateDatasourceModal({ open, setOpen, callback, initial
 	}, []);
 
 	return (
-		<Transition.Root show={open} as={Fragment}>
+		<Transition show={open} as={Fragment}>
 			<Dialog as='div' className='relative z-50' onClose={setOpen}>
-				<Transition.Child
+				<TransitionChild
 					as={Fragment}
 					enter='ease-out duration-300'
 					enterFrom='opacity-0'
@@ -35,11 +35,11 @@ export default function CreateDatasourceModal({ open, setOpen, callback, initial
 					leaveTo='opacity-0'
 				>
 					<div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
-				</Transition.Child>
+				</TransitionChild>
 
 				<div className='fixed inset-0 z-10 overflow-y-auto w-full'>
 					<div className='flex min-h-full items-end justify-center p-3 text-center sm:items-center sm:p-0 w-full'>
-						<Transition.Child
+						<TransitionChild
 							as={Fragment}
 							enter='ease-out duration-300'
 							enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
@@ -48,10 +48,10 @@ export default function CreateDatasourceModal({ open, setOpen, callback, initial
 							leaveFrom='opacity-100 translate-y-0 sm:scale-100'
 							leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
 						>
-							<Dialog.Panel className='relative transform rounded-lg bg-white px-3 pb-2 pt-4 text-left shadow-xl transition-all sm:my-8 sm:p-6 md:w-full lg:w-1/2 m-10 dark:bg-slate-800 dark:text-gray-50'>
-								<Dialog.Title as='h3' className='text-lg font-medium text-gray-900 dark:text-white'>
+							<DialogPanel className='relative transform rounded-lg bg-white px-3 pb-2 pt-4 text-left shadow-xl transition-all sm:my-8 sm:p-6 md:w-full lg:w-1/2 m-10 dark:bg-slate-800 dark:text-gray-50'>
+								<DialogTitle as='h3' className='text-lg font-medium text-gray-900 dark:text-white'>
 									Create a Datasource
-								</Dialog.Title>
+								</DialogTitle>
 								<div className='pt-4'>
 									<CreateDatasourceForm
 										compact={true}
@@ -61,11 +61,11 @@ export default function CreateDatasourceModal({ open, setOpen, callback, initial
 										initialStep={initialStep}
 									/>
 								</div>
-							</Dialog.Panel>
-						</Transition.Child>
+							</DialogPanel>
+						</TransitionChild>
 					</div>
 				</div>
 			</Dialog>
-		</Transition.Root>
+		</Transition>
 	);
 }
