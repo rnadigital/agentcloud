d43506d2d7f95b630e16401eb9555fac767ca3c7
Thomas Lynch <thomas@rna.digital>
2024-08-16 14:50:28 +1000

    Don't crash and instead disable server side posthog calls when missing the posthog token env

--- webapp/src/lib/posthog/index.ts
+++ webapp/src/lib/posthog/index.ts
@@ -1,7 +1,9 @@
 import { PostHog } from 'posthog-node';
 
-const client = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-	host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com'
-});
+const client =
+	process.env.NEXT_PUBLIC_POSTHOG_KEY &&
+	new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+		host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com'
+	});
 
 export default client;
