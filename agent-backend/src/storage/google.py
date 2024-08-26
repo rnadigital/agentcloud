63970fb786eaefd4e95427493d9a08877ea8546e
Ian Christopher B. de Jesus <3683356+iandjx@users.noreply.github.com>
2024-08-26 09:59:08 +0800

    Store file in file_folder

--- agent-backend/src/storage/google.py
+++ agent-backend/src/storage/google.py
@@ -29,27 +29,28 @@ class GoogleStorageProvider(StorageProvider):
             log.error(f'Failed to create GCS bucket: {e.message}')
             raise e
 
-    def upload_local_file(self, filename, is_public=False):
+    def upload_local_file(self, filename, folder_path, is_public=False):
         log.debug('Uploading file %s', filename)
         bucket_name = os.getenv('NEXT_PUBLIC_GCS_BUCKET_NAME' if is_public else 'NEXT_PUBLIC_GCS_BUCKET_NAME_PRIVATE')
         bucket = self.storage_client.bucket(bucket_name)
         
         original_file_path = os.path.join(Path(__file__).resolve().parent.parent, 'outputs', filename)  
-        blob = bucket.blob(filename)
+        blob = bucket.blob(f"{folder_path}/{filename}")
         try:
             blob.upload_from_filename(original_file_path)
             log.debug('File uploaded successfully.')
             if is_public:
                 blob.make_public()
+            os.remove(original_file_path)
         except Exception as err:
             log.error('File upload error:', err)
             raise err
 
-    async def delete_file(self, filename, is_public=False):
+    async def delete_file(self, filename, file_folder, is_public=False):
         log.debug('Deleting file %s', filename)
         bucket_name = os.getenv('NEXT_PUBLIC_GCS_BUCKET_NAME' if is_public else 'NEXT_PUBLIC_GCS_BUCKET_NAME_PRIVATE')
         bucket = self.storage_client.bucket(bucket_name)
-        blob = bucket.blob(filename)
+        blob = bucket.blob(f"{file_folder}/{filename}")
         blob.delete()
 
 google_storage_provider = GoogleStorageProvider()
--- agent-backend/src/storage/local.py
+++ agent-backend/src/storage/local.py
@@ -16,22 +16,26 @@ class LocalStorageProvider(StorageProvider):
         self.init()
         
 
-    async def init(self):
+    def init(self):
         Path(self.base_path).mkdir(parents=True, exist_ok=True)
 
-    async def upload_local_file(self, filename, is_public=False):
-        original_file_path = os.path.join(Path(__file__).resolve().parent.parent, filename)  # Open from original path
-        file_path = os.path.join(self.base_path, 'outputs', filename)  # Save to self.base_path
+    def upload_local_file(self, filename, file_folder, is_public=False):
+        
+        if not os.path.exists(os.path.join(self.base_path, file_folder)):
+            os.makedirs(os.path.join(self.base_path, file_folder))
+        original_file_path = os.path.join(Path(__file__).resolve().parent.parent,'outputs', filename)  # Open from original path
+        file_path = os.path.join(self.base_path, file_folder, filename)  # Save to self.base_path
         try:
             shutil.copyfile(original_file_path, file_path)
             log.debug(f"File '{filename}' uploaded successfully.")
+            os.remove(original_file_path)
         except Exception as e:
             log.error(f"Failed to upload file: {e}")
             raise e
 
 
-    async def delete_file(self, filename):
-        file_path = os.path.join(self.base_path, filename)
+    def delete_file(self, filename, file_folder):
+        file_path = os.path.join(self.base_path, file_folder, filename)
         try:
             os.remove(file_path)
             log.debug(f"File '{filename}' deleted successfully.")
