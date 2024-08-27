6682a31c0a181150902f1b4a3704d74fec90392b
ragy abraham <ragy@rna.digital>
2024-08-27 14:52:53 +1000

    merge develop

---
+++ webapp/src/hooks/session/useActiveTask.ts
@@ -0,0 +1,44 @@
+import * as API from '@api';
+import { useAccountContext } from 'context/account';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Task } from 'struct/task';
+
+const useActiveTask = (messages: any[]) => {
+	const [activeTask, setActiveTask] = useState<Task>();
+	const [accountContext]: any = useAccountContext();
+	const { csrf } = accountContext as any;
+	const lastRunningTaskName = messages.findLast(
+		m =>
+			typeof m.message?.text === 'string' &&
+			m?.message?.displayType === 'inline' && //optimization: task running messages are always an inline
+			m.message?.text?.startsWith('**Running task**:')
+	);
+	const router = useRouter();
+
+	const { resourceSlug } = router.query;
+
+	useEffect(() => {
+		if (lastRunningTaskName) {
+			const taskNameMatch = lastRunningTaskName.message.text.match(
+				/\*\*Running task\*\*:\s(.+?)\s\*\*/
+			);
+			const taskName = taskNameMatch ? taskNameMatch[1].trim() : null;
+
+			API.getTaskByName(
+				{
+					resourceSlug,
+					taskName,
+					_csrf: csrf
+				},
+				setActiveTask,
+				null,
+				null
+			);
+		}
+	}, [lastRunningTaskName]);
+
+	return activeTask;
+};
+
+export default useActiveTask;
