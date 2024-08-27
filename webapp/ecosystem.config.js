566891485560a4553dfc37418d7e2f58c8b44fb2
Thomas Lynch <thomas@rna.digital>
2024-08-27 12:09:24 +1000

    chore: format ecosystem config

--- webapp/ecosystem.config.js
+++ webapp/ecosystem.config.js
@@ -1,12 +1,23 @@
 module.exports = {
-  apps: [{
-    script: "npm run dev",
-    watch: true,
-    // watch_delay: 1000,
-    ignore_watch: ["node_modules", "src/components", "src/pages", "src/context", "\\.next", "\\.dist", "tsconfig.tsbuildinfo",".DS_Store","src/test"
-    ],
-    "env": {
-      "DEBUG": "webapp*,-webapp:middleware:auth:*",
-    },
-  }]
-}
+	apps: [
+		{
+			script: 'npm run dev',
+			watch: true,
+			// watch_delay: 1000,
+			ignore_watch: [
+				'node_modules',
+				'src/components',
+				'src/pages',
+				'src/context',
+				'\\.next',
+				'\\.dist',
+				'tsconfig.tsbuildinfo',
+				'.DS_Store',
+				'src/test'
+			],
+			env: {
+				DEBUG: 'webapp*,-webapp:middleware:auth:*'
+			}
+		}
+	]
+};
