5accba1b9273503864cef213d6951525cef119dc
Ian Christopher B. de Jesus <3683356+iandjx@users.noreply.github.com>
2024-09-11 08:03:05 +0800

    Scroll to highlighted option

--- webapp/src/components/variables/VariableDropdown.tsx
+++ webapp/src/components/variables/VariableDropdown.tsx
@@ -1,5 +1,6 @@
 import cn from 'lib/cn';
 import SelectClassNames from 'lib/styles/SelectClassNames';
+import { useEffect, useRef } from 'react';
 
 interface AutocompleteDropdownProps {
 	options: { label: string; value: string }[];
@@ -14,12 +15,25 @@ const AutocompleteDropdown = ({
 	dropdownPosition,
 	handleOptionSelect
 }: AutocompleteDropdownProps) => {
+	const listRef = useRef<HTMLUListElement>(null);
+
+	useEffect(() => {
+		const list = listRef.current;
+		if (list) {
+			const highlightedItem = list.children[highlightedIndex] as HTMLElement;
+			if (highlightedItem) {
+				highlightedItem.scrollIntoView({ block: 'nearest' });
+			}
+		}
+	}, [highlightedIndex]);
+
 	return (
 		<ul
+			ref={listRef}
 			className={cn(
 				'absolute list-none p-1 m-0 cursor-pointer z-10',
 				SelectClassNames.menu,
-				'w-fit'
+				'w-fit max-h-52 max-w-28, overflow-auto'
 			)}
 			style={{
 				top: dropdownPosition.top,
