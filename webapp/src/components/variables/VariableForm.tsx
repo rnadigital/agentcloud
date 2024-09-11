23911233c9d9d0ed3dc7b06904ab807e1ec93f7f
Ian Christopher B. de Jesus <3683356+iandjx@users.noreply.github.com>
2024-09-11 08:01:02 +0800

    Do not allow spaces in variable name

--- webapp/src/components/variables/VariableForm.tsx
+++ webapp/src/components/variables/VariableForm.tsx
@@ -79,10 +79,16 @@ export default function VariableForm({
 						<input
 							id='name'
 							type='text'
-							{...register('name', { required: true })}
+							{...register('name', {
+								required: 'Name is required',
+								pattern: {
+									value: /^\S*$/,
+									message: 'No spaces allowed'
+								}
+							})}
 							className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-slate-800 dark:ring-slate-600 dark:text-white ${errors.name ? 'border-red-500' : ''}`}
 						/>
-						{errors.name && <span className='text-red-500'>This field is required</span>}
+						{errors.name && <span className='text-red-500'>{errors.name.message as string}</span>}
 					</div>
 
 					<div>
