cdc916a7f1b8107ea034b6c825516d5ebaba884a
Nader <nader.awad@rna.digital>
2024-09-12 11:57:08 +1000

    lint

--- webapp/src/components/sharingmodes/WhiteListSharing.tsx
+++ webapp/src/components/sharingmodes/WhiteListSharing.tsx
@@ -5,9 +5,8 @@ import React, { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import Select from 'react-tailwindcss-select';
 import { SelectValue } from 'react-tailwindcss-select/dist/components/type';
-import SelectClassNames from 'styles/SelectClassNames';
 import { Option } from 'react-tailwindcss-select/dist/components/type';
-
+import SelectClassNames from 'styles/SelectClassNames';
 
 export interface WhiteListSharingValues {
 	emailsToShare: any;
@@ -94,59 +93,61 @@ export default function WhiteListSharing({
 							};
 							return (
 								<Select
-								value={emailState?.length > 0 ? emailState : null}
-								onChange={(v: any) => {
-									if (v?.some(val => val.value === null)) {
-										setModalOpen();
-										return;
-									}
-									if (v === null) {
-										onChange([]);
-										return;
-									}
-									onChange(v);
-								}}
-								primaryColor={'indigo'}
-								isMultiple
-								isSearchable
-								isClearable
-								placeholder='Select...'
-								classNames={SelectClassNames}
-								options={[
-									{
-										label: addNewTitle,
-										value: null,
-										disabled: false
-									},
-									{
-										label: 'Suggested members from your team:',
-										options: emailOptions
-									}
-								]}
-								formatOptionLabel={data => {
-									let optionAccount;
-									if (emailOptions) {
-										optionAccount = emailOptions.find(account => account.value === data.value);
-									}
-									return (
-										<li
-											className={`flex align-items-center !overflow-visible transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded hover:bg-blue-100 hover:text-blue-500 ${
-												data.isSelected ? 'bg-blue-100 text-blue-500' : 'dark:text-white'
-											}`}
-										>
-											<span className='ms-2 w-full overflow-hidden text-ellipsis'>{data.label}</span>
-										</li>
-									);
-								}}
-								formatGroupLabel={data => {
-									return (
-										<li className='flex align-items-center !overflow-visible transition duration-200 py-2 cursor-pointer select-none truncate rounded'>
-											<span className='border-t pt-3 ms-2 w-full overflow-hidden text-ellipsis'>
-												{data.label}
-											</span>
-										</li>
-									);
-								}}
+									value={emailState?.length > 0 ? emailState : null}
+									onChange={(v: any) => {
+										if (v?.some(val => val.value === null)) {
+											setModalOpen();
+											return;
+										}
+										if (v === null) {
+											onChange([]);
+											return;
+										}
+										onChange(v);
+									}}
+									primaryColor={'indigo'}
+									isMultiple
+									isSearchable
+									isClearable
+									placeholder='Select...'
+									classNames={SelectClassNames}
+									options={[
+										{
+											label: addNewTitle,
+											value: null,
+											disabled: false
+										},
+										{
+											label: 'Suggested members from your team:',
+											options: emailOptions
+										}
+									]}
+									formatOptionLabel={data => {
+										let optionAccount;
+										if (emailOptions) {
+											optionAccount = emailOptions.find(account => account.value === data.value);
+										}
+										return (
+											<li
+												className={`flex align-items-center !overflow-visible transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded hover:bg-blue-100 hover:text-blue-500 ${
+													data.isSelected ? 'bg-blue-100 text-blue-500' : 'dark:text-white'
+												}`}
+											>
+												<span className='ms-2 w-full overflow-hidden text-ellipsis'>
+													{data.label}
+												</span>
+											</li>
+										);
+									}}
+									formatGroupLabel={data => {
+										return (
+											<li className='flex align-items-center !overflow-visible transition duration-200 py-2 cursor-pointer select-none truncate rounded'>
+												<span className='border-t pt-3 ms-2 w-full overflow-hidden text-ellipsis'>
+													{data.label}
+												</span>
+											</li>
+										);
+									}}
 								/>
 							);
 						}}
