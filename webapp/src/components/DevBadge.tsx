791325233fbd38e1e89c9d0e7771752d1643b478
Thomas Lynch <thomas@rna.digital>
2024-09-19 12:00:02 +1000

    Make developer mode badges have a copy icon, change color on hover, copy to clipboad on click, and not click through if theyre nested within another element with a click handler.

--- webapp/src/components/DevBadge.tsx
+++ webapp/src/components/DevBadge.tsx
@@ -1,17 +1,30 @@
+import { ClipboardDocumentIcon } from '@heroicons/react/24/outline';
 import { useDeveloperContext } from 'context/developer';
+import { toast } from 'react-toastify';
 
 interface DevBadgeProps {
-	text: string;
+	label?: string;
+	value: string;
 }
 
-export default function DevBadge({ text }: DevBadgeProps) {
+export default function DevBadge({ label = 'ID', value }: DevBadgeProps) {
 	const { developerMode } = useDeveloperContext();
 	if (!developerMode) {
 		return null;
 	}
 	return (
-		<span className='whitespace-nowrap h-6 px-2 py-[0.5px] border text-sm rounded-lg bg-blue-100 text-blue-800 border-blue-300'>
-			{text}
+		<span
+			onClick={async e => {
+				e.preventDefault();
+				e.stopPropagation();
+				await navigator.clipboard.writeText(value);
+				toast.success('Copied to clipboard');
+				return false;
+			}}
+			className='hover:bg-blue-200 transition-all whitespace-nowrap h-6 px-2 py-[0.5px] border text-sm rounded-lg bg-blue-100 text-blue-800 border-blue-300'
+		>
+			{label}: {value}
+			<ClipboardDocumentIcon aria-hidden='true' className='inline ms-1 -mt-1 h-4 w-4' />
 		</span>
 	);
 }
