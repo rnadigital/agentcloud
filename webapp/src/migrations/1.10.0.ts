fb2425d483a082ecc650157cd41360d654ba4c84
Thomas Lynch <thomas@rna.digital>
2024-09-20 15:59:56 +1000

    Revert "Bugfix some more areas where potentially not having .variableIds set on old/existing agents/tasks will break"
    This reverts commit c5251397a7bed59941bfef7c00023cbc4577ca55.

--- webapp/src/migrations/1.10.0.ts
+++ webapp/src/migrations/1.10.0.ts
@@ -1,22 +1,37 @@
+import Permission from '@permission';
 import debug from 'debug';
+import { Binary } from 'mongodb';
+import Roles from 'permissions/roles';
+
 const log = debug('webapp:migration:1.10.0');
 
+async function updateTeamMemberPermissions(db) {
+	const teamMemberPermissions = new Permission();
+	teamMemberPermissions.setAll(Roles.TEAM_MEMBER.array);
+	const accounts = await db.collection('accounts').find().toArray();
+	for (const account of accounts) {
+		const accountPermissions = new Permission(account.permissions.buffer);
+		const isTeamMember = Roles.TEAM_MEMBER.array.some(bit => accountPermissions.get(bit));
+		if (!isTeamMember) {
+			continue;
+		}
+		const hasAllTeamMemberPermissions = Roles.TEAM_MEMBER.array.every(bit =>
+			accountPermissions.get(bit)
+		);
+		if (!hasAllTeamMemberPermissions) {
+			accountPermissions.setAll(teamMemberPermissions.array);
+			await db
+				.collection('accounts')
+				.updateOne(
+					{ _id: account._id },
+					{ $set: { permissions: new Binary(accountPermissions.array) } }
+				);
+		}
+	}
+}
+
 export default async function (db) {
-	log('adding variableIds to all tasks and agents');
-	await db.collection('tasks').updateMany(
-		{},
-		{
-			$set: {
-				variableIds: []
-			}
-		}
-	);
-	await db.collection('agents').updateMany(
-		{},
-		{
-			$set: {
-				variableIds: []
-			}
-		}
-	);
+	log('Starting migration to update TEAM_MEMBER permissions');
+	await updateTeamMemberPermissions(db);
+	log('Migration to update TEAM_MEMBER permissions completed');
 }
