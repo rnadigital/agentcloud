43b0f0ec4a6a64fa1c86f46639e48757a6041ccf
NaderRNA <nader.awad@rna.digital>
2024-10-16 09:44:47 +1100

    modifying migration to correctly add tool as option to install for builtins

--- webapp/src/migrations/1.12.0.ts
+++ webapp/src/migrations/1.12.0.ts
@@ -1,16 +1,18 @@
 import debug from 'debug';
 import { CollectionName } from 'lib/struct/db';
 import { ObjectId } from 'mongodb';
+import { ToolType } from 'struct/tool';
 const log = debug('webapp:migration:1.12.0');
 
 export default async function (db) {
-	log('Add new builtin firecrawl html scraper tool to each team');
+	log('Add new builtin firecrawl html scraper tool to tools db');
 
-	// Define the new tool object
-	const newTool = {
+	//add the new tool into the db with no teamId or orgId (that should default it to every org and team as an option to add)
+	await db.collection('tools').insertOne({
+		_id: new ObjectId(),
 		name: 'Firecrawl Loader',
 		description: 'This tool takes a url and parses the page for all text content',
-		type: 'builtin', // Corresponds to ToolType.BUILTIN_TOOL
+		type: ToolType.BUILTIN_TOOL, // Corresponds to ToolType.BUILTIN_TOOL
 		data: {
 			name: 'firecrawl_loader',
 			description: 'This tool takes a url and parses the page for all text content',
@@ -36,18 +38,5 @@ export default async function (db) {
 				}
 			}
 		}
-	};
-
-	// Fetch all teams
-	const teams = await db.collection('teams').find({}).toArray();
-
-	// Insert the new tool for each team
-	for (const team of teams) {
-		await db.collection('tools').insertOne({
-			...newTool,
-			_id: new ObjectId(),
-			orgId: team.orgId,
-			teamId: team._id
-		});
-	}
+	});
 }
