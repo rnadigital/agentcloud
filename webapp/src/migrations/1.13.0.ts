b25968293524a2b9593715343890ba19e3c469c8
NaderRNA <nader.awad@rna.digital>
2024-10-29 08:10:16 +1100

    sligtly modifying migration to improve performance significantly

--- webapp/src/migrations/1.13.0.ts
+++ webapp/src/migrations/1.13.0.ts
@@ -6,25 +6,40 @@ import { TEAM_MEMBER } from 'permissions/roles';
 const log = debug('webapp:migration:1.13.0');
 
 export default async function updateTeamMemberPermissions(db) {
-	log('add variable permissions to team members');
+	log('adding variable permissions to team members');
+
 	const teamMemberPermissions = new Permission();
 	teamMemberPermissions.setAll(TEAM_MEMBER.array);
-	const accounts = await db.collection('accounts').find().toArray();
+
+	// Retrieve only `_id` and `permissions` fields for efficiency
+	const accounts = await db
+		.collection('accounts')
+		.find({}, { projection: { _id: 1, permissions: 1 } })
+		.toArray();
+
+	const teamMembersWithoutPermissionsIdArray = [];
+
 	for (const account of accounts) {
 		const accountPermissions = new Permission(account.permissions.buffer);
+
 		const isTeamMember = TEAM_MEMBER.array.some(bit => accountPermissions.get(bit));
 		if (!isTeamMember) {
 			continue;
 		}
+
 		const hasAllTeamMemberPermissions = TEAM_MEMBER.array.every(bit => accountPermissions.get(bit));
 		if (!hasAllTeamMemberPermissions) {
-			accountPermissions.setAll(teamMemberPermissions.array);
-			await db
-				.collection('accounts')
-				.updateOne(
-					{ _id: account._id },
-					{ $set: { permissions: new Binary(accountPermissions.array) } }
-				);
+			teamMembersWithoutPermissionsIdArray.push(account._id);
 		}
+	}
+
+	// Perform batch update for all team members missing required permissions
+	if (teamMembersWithoutPermissionsIdArray.length > 0) {
+		await db
+			.collection('accounts')
+			.updateMany(
+				{ _id: { $in: teamMembersWithoutPermissionsIdArray } },
+				{ $set: { permissions: new Binary(teamMemberPermissions.array) } }
+			);
 	}
 }
