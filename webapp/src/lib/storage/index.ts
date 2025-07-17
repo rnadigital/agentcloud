5913a285dc9ec8f71826d5a375ed538d049016fc
Ragy Abraham <ragy@rna.digital>
2025-07-18 09:43:46 +1000

    reverting storage provider factory code

--- webapp/src/lib/storage/index.ts
+++ webapp/src/lib/storage/index.ts
@@ -1,20 +1,11 @@
+import GoogleStorageProvider from 'storage/google';
 import LocalStorageProvider from 'storage/local';
-
-let GoogleStorageProvider: any = null;
-
-if (typeof window === 'undefined') {
-	GoogleStorageProvider = require('storage/google').default;
-}
 
 export default class StorageProviderFactory {
 	static getStorageProvider(providerName?: string) {
 		const provider = providerName || process.env.NEXT_PUBLIC_STORAGE_PROVIDER;
 		switch ((provider || 'local').toLowerCase()) {
 			case 'google':
-				if (!GoogleStorageProvider) {
-					console.warn('Google Storage not available on client side, falling back to local');
-					return LocalStorageProvider;
-				}
 				return GoogleStorageProvider;
 			case 'local':
 				return LocalStorageProvider;
